8bd58c69adb5c03a0d0d72578e8d9dee43feff2b
Kamila Luczaj <kamilcia1417@wp.pl>
2022-03-10 00:00:45 +0100

    Changed to the allowed hosts list

--- backend_first/settings.py
+++ backend_first/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://arcane-spire-03245.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'arcane-spire-03245.herokuapp.com/']
 
 
 # Application definition
