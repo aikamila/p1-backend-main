309910a5d010613cf32a024b5e1253deb0671602
Kamila Luczaj <kamilcia1417@wp.pl>
2022-03-10 16:02:59 +0100

    Add allowed hosts

--- backend_first/settings.py
+++ backend_first/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['127.0.0.1', 'arcane-spire-03245.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'arcane-spire-03245.herokuapp.com', 'shangify.com']
 
 
 # Application definition
